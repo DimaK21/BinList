20199aaec2f38dd7fe002aa9ef33121b57ef8492
Dmitry Kryushnikov <civ02@mail.ru>
2025-01-09 22:39:23 +0500

    di для Retrofit

--- app/src/main/java/ru/kryu/binlist/di/AppModule.kt
+++ app/src/main/java/ru/kryu/binlist/di/AppModule.kt
@@ -7,10 +7,14 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import ru.kryu.binlist.data.CardIfoRepositoryImpl
 import ru.kryu.binlist.data.CardListRepositoryImpl
 import ru.kryu.binlist.data.database.CardDetailsDao
 import ru.kryu.binlist.data.database.CardDetailsDatabase
+import ru.kryu.binlist.data.network.BinApi
+import ru.kryu.binlist.data.network.RetrofitNetworkClient
 import ru.kryu.binlist.domain.CardIfoRepository
 import ru.kryu.binlist.domain.CardListRepository
 import javax.inject.Singleton
@@ -21,8 +25,11 @@ class AppModule {
 
     @Provides
     @Singleton
-    fun provideCardIfoRepository(cardDetailsDao: CardDetailsDao): CardIfoRepository =
-        CardIfoRepositoryImpl(cardDetailsDao)
+    fun provideCardIfoRepository(
+        cardDetailsDao: CardDetailsDao,
+        retrofitNetworkClient: RetrofitNetworkClient,
+    ): CardIfoRepository =
+        CardIfoRepositoryImpl(cardDetailsDao, retrofitNetworkClient)
 
     @Provides
     @Singleton
@@ -38,5 +45,23 @@ class AppModule {
     @Provides
     fun provideEventDao(database: CardDetailsDatabase): CardDetailsDao {
         return database.cardDetailsDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofitNetworkClient(binApi: BinApi): RetrofitNetworkClient =
+        RetrofitNetworkClient(binApi)
+
+    @Provides
+    @Singleton
+    fun provideBinApi(): BinApi =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(BinApi::class.java)
+
+    companion object {
+        private const val BASE_URL = "https://lookup.binlist.net/"
     }
 }
